1ad3ef1d16bdcb5989daf65a27f4935554f2148b
TheTensaii <f.salimou@hotmail.fr>
2025-07-19 15:49:39 -0400

    chore: setup vscode settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Canvas} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Cube } from './Cube'
 
 function App() {
-  
-
   return (
     <Canvas>
       <Cube />
--- src/Cube.tsx
+++ src/Cube.tsx
@@ -3,18 +3,18 @@ import { useRef } from "react";
 import type { Mesh } from "three";
 
 export const Cube = () => {
-  const cube = useRef<Mesh>(null)
-    
+  const cubeRef = useRef<Mesh>(null)
+
   useFrame((_state, delta) => {
-    if(cube.current){
-      cube.current.rotation.y += 2*delta; 
+    if (cubeRef.current) {
+      cubeRef.current.rotation.y += 2 * delta;
     }
   })
 
   return (
-    <mesh ref={cube}>
+    <mesh ref={cubeRef}>
       <boxGeometry />
-      <meshBasicMaterial color="blue"/>
+      <meshBasicMaterial color="blue" />
     </mesh>
   )
 }
