bd97ae67fe5b1f50092f3273204cfe39c2c1d8c3
TheTensaii <f.salimou@hotmail.fr>
2025-07-27 15:21:47 -0400

    feat: display shoot direction arrow

--- src/ShootingPlane.tsx
+++ src/ShootingPlane.tsx
@@ -1,5 +1,6 @@
 import type { ThreeEvent } from "@react-three/fiber"
-import { DoubleSide, Vector3 } from "three"
+import { useRef } from "react"
+import { DoubleSide, Mesh, Vector3 } from "three"
 
 type ShootingPlanProps = {
   position: Vector3
@@ -8,15 +9,50 @@ type ShootingPlanProps = {
 
 export const ShootingPlane = ({ position, onShoot }: ShootingPlanProps) => {
 
+  const arrowRef = useRef<Mesh>(null)
+
   const handleShoot = (event: ThreeEvent<PointerEvent>) => {
+    hideArrow()
     const x = -((event.uv?.x ?? 0.5) - 0.5) * 1.5
     const y = Math.max((event.uv?.y ?? 0.5) - 0.5, 0.02) * 1.5
 
     onShoot(x, y)
   }
 
-  return <mesh position={position} rotation={[0, Math.PI, 0]} scale={5} onPointerUp={handleShoot}>
-    <planeGeometry />
-    <meshBasicMaterial color={"red"} side={DoubleSide} wireframe />
-  </mesh>
+  const displayArrow = () => {
+    if (arrowRef.current) {
+      arrowRef.current.visible = true
+    }
+  }
+
+  const moveArrow = (event: ThreeEvent<PointerEvent>) => {
+    const x = -((event.uv?.x ?? 0.5) - 0.5) * 1.5
+    const y = Math.max((event.uv?.y ?? 0.5) - 0.5, 0.02) * 1.5
+
+    if (arrowRef.current) {
+      arrowRef.current.lookAt(new Vector3(x, y, 0.3).add(position))
+    }
+  }
+
+  const hideArrow = () => {
+    if (arrowRef.current) {
+      arrowRef.current.visible = false
+    }
+  }
+
+  return (
+    <>
+      <mesh position={position} rotation={[0, Math.PI, 0]} scale={5} onPointerDown={displayArrow} onPointerMove={moveArrow} onPointerUp={handleShoot}>
+        <planeGeometry />
+        <meshBasicMaterial side={DoubleSide} wireframe />
+      </mesh>
+      <group ref={arrowRef} position={position} visible={false}>
+        <mesh position={[0, 0, 0.5]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.03, 0.03, 1, 20]} />
+          <meshBasicMaterial color={"blue"} side={DoubleSide} />
+        </mesh>
+      </group>
+    </>
+
+  )
 }
