fefe886508f37d553492821debff1f95c5e25afd
TheTensaii <f.salimou@hotmail.fr>
2025-12-30 12:02:42 -0500

    feat: keep db between deployments

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,9 +1,10 @@
 import SQLite from 'better-sqlite3'
 import { Kysely, SqliteDialect } from 'kysely'
 import { GameTable } from './models/Game.js'
+import { Environment } from '../environment.js'
 
 export const dialect = new SqliteDialect({
-  database: new SQLite(':memory:'),
+  database: new SQLite(Environment.DB_FILENAME),
 })
 
 export interface Database {
--- api/src/environment.ts
+++ api/src/environment.ts
@@ -1,5 +1,6 @@
 export const Environment = {
   HOST: process.env.HOST,
   PORT: Number(process.env.PORT),
-  WEBAPP_URL: process.env.WEBAPP_URL
+  WEBAPP_URL: process.env.WEBAPP_URL,
+  DB_FILENAME: process.env.DB_FILENAME
 } as const
