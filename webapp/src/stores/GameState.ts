56a0594f13f29d0c5da36f4d35bc5f0e4565f251
TheTensaii <f.salimou@hotmail.fr>
2026-01-11 21:30:42 -0500

    refact: remove isShooting from state manager

--- webapp/src/stores/GameState.ts
+++ webapp/src/stores/GameState.ts
@@ -7,14 +7,12 @@ interface GameState {
   score: number,
   bestScore?: number,
   playerName?: string,
-  isShooting: boolean,
   startTime: number,
   lastBucketTime: number,
   endTime: number,
   startNewGame: () => void
   endGame: () => void
   scoreBucket: () => void
-  setIsShooting: (value: boolean) => void
   setBestScore: (value: number) => void
   setPlayerName: (value: string) => void
 }
@@ -22,14 +20,12 @@ interface GameState {
 export const useGameState = create<GameState>((set) => ({
   phase: "ready",
   score: 0,
-  isShooting: false,
   startTime: Date.now(),
   lastBucketTime: Date.now(),
   endTime: Date.now(),
-  startNewGame: () => set(() => ({ phase: 'playing', score: 0, isShooting: false, startTime: Date.now(), lastBucketTime: Date.now() })),
-  endGame: () => set(() => ({ phase: 'ended', isShooting: false, endTime: Date.now() })),
+  startNewGame: () => set(() => ({ phase: 'playing', score: 0, startTime: Date.now(), lastBucketTime: Date.now() })),
+  endGame: () => set(() => ({ phase: 'ended', endTime: Date.now() })),
   scoreBucket: () => set((state) => ({ score: state.score + 1, lastBucketTime: Date.now() })),
-  setIsShooting: (value: boolean) => set(() => ({ isShooting: value })),
   setBestScore: (value: number) => set(() => ({ bestScore: value })),
   setPlayerName: (value: string) => set(() => ({ playerName: value })),
 }))
