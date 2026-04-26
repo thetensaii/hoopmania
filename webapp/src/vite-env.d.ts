171e6efd9b3be4cd86ae3a29d9ce250e118ae67c
TheTensaii <f.salimou@hotmail.fr>
2026-04-26 16:15:01 -0400

    feat: add pausible endpoint

--- webapp/src/vite-env.d.ts
+++ webapp/src/vite-env.d.ts
@@ -9,6 +9,7 @@ interface ViteTypeOptions {
 interface ImportMetaEnv {
   readonly VITE_WEBAPP_URL: string
   readonly VITE_API_URL: string
+  readonly VITE_PLAUSIBLE_ENDPOINT_URL: string
 }
 
 interface ImportMeta {
