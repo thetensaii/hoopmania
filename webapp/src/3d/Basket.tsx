861c2907ca33da6bafc7f2aee602206a0e2d4a75
TheTensaii <f.salimou@hotmail.fr>
2026-09-12 18:50:46 -0400

    feat: change score style

--- webapp/src/3d/Basket.tsx
+++ webapp/src/3d/Basket.tsx
@@ -1,9 +1,8 @@
 import { CuboidCollider, MeshCollider, RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { useRef, type RefObject } from "react";
+import { type RefObject } from "react";
 import * as THREE from "three"
 import { useBasketActions } from "../hooks/3d/useBasketActions";
-import { Html, useTexture, Text3D, Center, } from "@react-three/drei";
-import { css } from "../../styled-system/css";
+import { useTexture, Text3D, Center, } from "@react-three/drei";
 import { useGetTimeLeftInSec } from "../hooks/useGetTimeLeft";
 
 const basketDiameter_M = 0.07
@@ -20,7 +19,6 @@ type BasketProps = {
 export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) => {
   useBasketActions(ref)
   const timeLeft = useGetTimeLeftInSec()
-  const clockRef = useRef<THREE.Mesh>(null)
 
   const backboardTexture = useTexture('./textures/backboard.jpg')
   backboardTexture.colorSpace = THREE.SRGBColorSpace
@@ -35,7 +33,7 @@ export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) =
         <MeshCollider type='trimesh'>
           <mesh rotation-x={Math.PI / 2}>
             <torusGeometry args={[basketDiameter_M * 10 / 2, 0.02]} />
-            <meshBasicMaterial color={[5, 0, 0]} />
+            <meshBasicMaterial color={[2, 0, 0]} />
           </mesh>
         </MeshCollider>
 
@@ -43,13 +41,12 @@ export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) =
           <MeshCollider type='trimesh'>
             <mesh >
               <planeGeometry args={[backboardLength_M * 10, backboardHeight_M * 10]} />
-              <meshBasicMaterial color={[0, 0, 12]} transparent alphaMap={backboardTexture} />
+              <meshBasicMaterial color={[0, 0, 4]} transparent alphaMap={backboardTexture} />
             </mesh>
           </MeshCollider>
 
           <Center position={[0, 1, 0]}>
             <Text3D
-              ref={clockRef}
               font={'./fonts/ds_digital/DS-Digital_Normal.json'}
               size={0.5}
               height={0.00000001}
@@ -58,27 +55,20 @@ export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) =
               <meshBasicMaterial color={[5, 0, 0]} />
             </Text3D>
           </Center>
+
+          <Center position={[0, 0.3, 0]}>
+            <Text3D
+              font={'./fonts/ds_digital/DS-Digital_Normal.json'}
+              size={0.2}
+              height={0.00000001}
+            >
+              {score}
+              <meshBasicMaterial color={[5, 1, 0]} />
+            </Text3D>
+          </Center>
         </group>
 
         <CuboidCollider args={[basketDiameter_M / 2, 0.1, basketDiameter_M / 2]} position={[0, -0.3, 0]} onIntersectionEnter={handleBucket} sensor={true} />
-        <Html
-          position={[0, backboardHeight_M * 10 / 2, -basketDiameter_M * 10 / 2]}
-          zIndexRange={[0, 0]}
-          center
-          transform
-          occlude='blending'
-          wrapperClass={css({
-            bg: '[#A9A9A9]',
-            pointerEvents: 'none',
-            userSelect: 'none',
-          })}
-          className={css({
-            color: 'darkBlue.900',
-            fontSize: 'text.body1'
-          })}
-        >
-          {score}
-        </Html>
       </group>
     </RigidBody >
   )
