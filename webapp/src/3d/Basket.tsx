3cd104dcda0329160dcc1c91c90fc2db7568658d
TheTensaii <f.salimou@hotmail.fr>
2026-09-12 20:15:32 -0400

    feat: bloom basket on scoring

--- webapp/src/3d/Basket.tsx
+++ webapp/src/3d/Basket.tsx
@@ -1,9 +1,11 @@
 import { CuboidCollider, MeshCollider, RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { type RefObject } from "react";
+import { useRef, type RefObject } from "react";
 import * as THREE from "three"
 import { useBasketActions } from "../hooks/3d/useBasketActions";
 import { useTexture, Text3D, Center, } from "@react-three/drei";
 import { useGetTimeLeftInSec } from "../hooks/useGetTimeLeft";
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap'
 
 const basketDiameter_M = 0.07
 const backboardLength_M = 0.183
@@ -19,19 +21,57 @@ type BasketProps = {
 export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) => {
   useBasketActions(ref)
   const timeLeft = useGetTimeLeftInSec()
+  const hoopMeshRef = useRef<THREE.Mesh>(null)
+  const backboardMeshRef = useRef<THREE.Mesh>(null)
+  const { contextSafe } = useGSAP()
 
   const backboardTexture = useTexture('./textures/backboard.jpg')
   backboardTexture.colorSpace = THREE.SRGBColorSpace
 
+  const triggerBucketAnimation = contextSafe(() => {
+    if (hoopMeshRef.current?.material instanceof THREE.MeshBasicMaterial) {
+      gsap.fromTo(hoopMeshRef.current.material.color, {
+        r: 2,
+        g: 0,
+        b: 0,
+      }, {
+        r: 8,
+        g: 0,
+        b: 0,
+        duration: 0.5,
+        repeat: 1,
+        ease: "none",
+        yoyo: true
+      })
+    }
+    if (backboardMeshRef.current?.material instanceof THREE.MeshBasicMaterial) {
+      gsap.fromTo(backboardMeshRef.current.material.color, {
+        r: 0,
+        g: 0,
+        b: 4,
+      }, {
+        r: 0,
+        g: 4,
+        b: 0,
+        duration: 0.5,
+        repeat: 1,
+        ease: "none",
+        yoyo: true
+      })
+    }
+
+  })
+
   const handleBucket = () => {
     onBucket()
+    triggerBucketAnimation()
   }
 
   return (
     <RigidBody ref={ref} type="kinematicPosition" colliders={false} position={initialPosition}>
       <group>
         <MeshCollider type='trimesh'>
-          <mesh rotation-x={Math.PI / 2}>
+          <mesh ref={hoopMeshRef} rotation-x={Math.PI / 2}>
             <torusGeometry args={[basketDiameter_M * 10 / 2, 0.02]} />
             <meshBasicMaterial color={[2, 0, 0]} />
           </mesh>
@@ -39,7 +79,7 @@ export const Basket = ({ ref, initialPosition, onBucket, score }: BasketProps) =
 
         <group position={[0, backboardHeight_M * 10 / 2, -basketDiameter_M * 10 / 2]}>
           <MeshCollider type='trimesh'>
-            <mesh >
+            <mesh ref={backboardMeshRef}>
               <planeGeometry args={[backboardLength_M * 10, backboardHeight_M * 10]} />
               <meshBasicMaterial color={[0, 0, 4]} transparent alphaMap={backboardTexture} />
             </mesh>
