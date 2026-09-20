c974d10fd3d5ade6e21c7e6afa5462a9aaef2103
TheTensaii <f.salimou@hotmail.fr>
2026-09-19 21:19:42 -0400

    fix: improve perf by reducing grid vertices

--- webapp/src/3d/Background/Grid/Grid.tsx
+++ webapp/src/3d/Background/Grid/Grid.tsx
@@ -30,7 +30,7 @@ export const Grid = () => {
 
   return (
     <mesh position={[0, -5.3, -5]} rotation={[Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[WIDTH, HEIGHT, 100, 2000]} />
+      <planeGeometry args={[WIDTH, HEIGHT, 100, 100]} />
       <rawShaderMaterial
         ref={materialRef}
         uniforms={uniforms}
